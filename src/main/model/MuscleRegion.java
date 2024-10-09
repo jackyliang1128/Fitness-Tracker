abdd73dd5083301eba5a3cb56bff63644f579f92
jackyliang1128 <55817542+jackyliang1128@users.noreply.github.com>
2024-10-09 16:45:26 -0700

    Added specification for MuscleRegion class

---
+++ src/main/model/MuscleRegion.java
@@ -0,0 +1,16 @@
+package model;
+
+// Represents a major muscle region focussed by the exercise
+public enum MuscleRegion {
+    CHEST,
+    BACK,
+    SHOULDER,
+    BICEPS,
+    TRICEPS,
+    QUADRICEPS,
+    HAMSTRING,
+    CALFS,
+    GLUTES,
+    CORE,
+
+}
