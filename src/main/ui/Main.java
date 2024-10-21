2a5ae4ce35e52152c032d04b9cf3da49c2eb2794
jackyliang1128 <55817542+jackyliang1128@users.noreply.github.com>
2024-10-21 16:21:00 -0700

    Added specification for Writable interface

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -1,5 +1,6 @@
 package ui;
 
+// the main code and runs the FitnessApp
 public class Main {
     public static void main(String[] args) throws Exception {
         System.out.println("\nWelcome to the Fitness Tracker");
