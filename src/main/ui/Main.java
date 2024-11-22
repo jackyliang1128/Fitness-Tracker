df3b9cae874e580bd48b8e67b788d230b26429dc
jackyliang1128 <55817542+jackyliang1128@users.noreply.github.com>
2024-11-22 02:56:17 -0800

    Format to pass checkstyle

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -6,19 +6,16 @@ import java.io.FileNotFoundException;
 public class Main {
     public static void main(String[] args) {
 
-
         // uncomment to run console version from phase 2
         // try {
-        //     System.out.println("\nWelcome to the Fitness Tracker");
-        //     new FitnessApp();
+        // System.out.println("\nWelcome to the Fitness Tracker");
+        // new FitnessApp();
         // } catch (FileNotFoundException e) {
-        //     System.out.println("Unable to run application: file not found");
+        // System.out.println("Unable to run application: file not found");
         // }
 
         // uncomment to run GUI version from phase 3
         new FitnessAppGUI();
-
-        
 
     }
 }
